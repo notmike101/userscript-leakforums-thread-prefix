215e3e4d1281541ef65a5bdf247e08904ca71332
Mike Orozco <mike.orozco94+gitlab@gmail.com>
2016-02-29 01:39:45 +0000

    Initial Version

---
+++ lf-thread-prefix.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           LeakForums All Thread Prefixes
+// @version        1.0.0
+// @description    Allow for the use of all avaliable thread prefixes
+// @downloadURL    https://gitlab.com/notmike101/leakforums-thread-prefix/raw/master/lf-thread-prefix.user.js
+// @updateURL      https://gitlab.com/notmike101/leakforums-thread-prefix/raw/master/lf-thread-prefix.meta.js
+//
+// @author         Mike Orozco / IRDeNial / R00D
+// @namespace      leakforums/allthredprefix
+//
+// @include        *://leakforums.net/editpost.php?*
+// @include        *://www.leakforums.net/editpost.php?*
+// @include        *://*.leakforums.net/editpost.php*
+// @include        *://leakforums.net/newthread.php?*
+// @include        *://www.leakforums.net/newthread.php?*
+// @include        *://*.leakforums.net/newthread.php*
+//
+// @grant          metadata
+// ==/UserScript==
